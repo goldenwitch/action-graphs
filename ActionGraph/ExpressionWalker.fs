5a0985854159af431b24db6b9667971ec5d37427
Goldenwitch <autumnwyborny@witchfolio.com>
2020-11-23 13:12:43 -0800

    Initial commit

---
+++ ActionGraph/ExpressionWalker.fs
@@ -0,0 +1,44 @@
+﻿namespace Graph
+
+module Expressions = 
+    type Walker<'T> =
+        {
+            mutable CurrentNode : Node<'T>
+        }
+
+        member this.Walk(graph : Graph<'T>, expression : string) =
+            //Accepts graph and expression
+            //Expression in form of "Edge.Edge.Edge" OR "Edge=1.Edge.Edge=5"
+            for label in expression.Split(".") do
+                //Split out params
+                let valueSplit = label.Split("=")
+                if valueSplit.Length <= 1 then
+                    this.CurrentNode <- graph.WalkEdge(this.CurrentNode.Id, StringValue(label))
+                else
+                    this.CurrentNode <- graph.WalkEdge(this.CurrentNode.Id, FuncValue((StringValue(valueSplit.[0]), StringValue(valueSplit.[1]))))
+    
+    type SuperWalker<'T> =
+        {
+            CurrentNode : Node<'T>
+            SuperEdges : Map<string, (Walker<'T> * GraphValue -> Unit)>
+        }
+
+        member this.Walk(graph : Graph<'T>, expression : string) =
+            let baseWalker : Walker<'T> = 
+                {
+                    CurrentNode = this.CurrentNode
+                }
+        
+            for label in expression.Split(".") do
+                let valueSplit = label.Split("=")
+                if valueSplit.Length <= 1 then
+                    if this.SuperEdges.ContainsKey(label) then //If the next item on the expression matches an item from the superedges, run the super edge
+                        this.SuperEdges.[label](baseWalker, StringValue(label))
+                    else //Else run the normal walker on the remaining components of the expression
+                        baseWalker.Walk(graph, label)
+                else
+                    if this.SuperEdges.ContainsKey(valueSplit.[0]) then //If the next item on the expression matches an item from the superedges, run the super edge
+                        this.SuperEdges.[valueSplit.[0]](baseWalker, StringValue(valueSplit.[1]))
+                    else //Else run the normal walker on the remaining components of the expression
+                        baseWalker.Walk(graph, label)
+            ()
---
+++ ActionGraph/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Graph
+open Graph.Expressions
+open Graph.MathGraph
+
+let consoleLoop (args:string) =
+    let walker =
+        {
+            CurrentNode = MathGraph.Definition.Nodes.[StringValue("Math")]
+        }
+    walker.Walk(MathGraph.Definition, "add=2.Exponent=1.add.add.multiply=5")
+    ()
+
+let rec result() =
+    let readValue = Console.ReadLine()
+    if not(String.IsNullOrWhiteSpace readValue)
+    then
+        consoleLoop readValue
+        result()
+    else
+        consoleLoop readValue
+
+[<EntryPoint>]
+let main argv =
+    let message = "Hello Autumn"
+    Console.WriteLine message
+
+    result()
+    0 // return an integer exit code
