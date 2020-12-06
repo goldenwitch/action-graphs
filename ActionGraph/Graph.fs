f768450ecc6113da6bea1e52e1c2561a5febc135
Autumn Wyborny <autumnwyborny@ddmobileservices.com>
2020-12-06 13:51:25 -0600

    made graph edges reliably referential

--- ActionGraph/Graph.fs
+++ ActionGraph/Graph.fs
@@ -1,6 +1,36 @@
 ﻿namespace ActionGraph
 open System
-open Newtonsoft.Json.Linq
+open System.Text
+open System.Linq
+
+type NavStep =
+    | Up
+    | Down of string
+    | End
+type TargetingExpression =
+    {
+        NavSteps : seq<NavStep>
+        Target : string
+    }
+
+module TargetParser =
+    let ExtractTarget(input:string) =
+        //Split on < and >
+        let array = input.Split('<','>')
+        //project array -> navsteps
+        {
+            NavSteps =
+                seq{
+                    for i in 0 .. array.Length-1 do
+                        if i = array.Length-1 then
+                            End
+                        else if array.[i].Length > 0 then
+                            Down(array.[i])
+                        else
+                            Up
+                }
+            Target = array.Last()
+        }
 
 type GraphValue =
     | StringValue of string
@@ -32,18 +62,99 @@ type GraphValue =
 type Edge =
     {
         Id : GraphValue
-        Action : (Node * Node * GraphValue * Graph -> unit)
+        Action : String
         To : GraphValue
     }
-and Node =
+type ExpressionEdge =
     {
         Id : GraphValue
-        Edges : Map<GraphValue, Edge>
+        Action : String //Replace with 'Action' Type later, that validates the incoming string and offers debug messages
+    }
+type EdgeLike =
+| Edge of Edge
+| ExpressionEdge of ExpressionEdge
+    
+    member x.Walk(fromNode:Node, graph: Graph, velocity:GraphValue) =
+        match x with
+        | Edge a ->
+            let doWalk(toNode, targetGraph) = 
+                if(targetGraph.EdgeActions.ContainsKey(a.Action)) then
+                    match targetGraph.EdgeActions.[a.Action] with 
+                    | ActionEdge e ->
+                        e(fromNode, targetGraph.Nodes.[toNode], velocity, targetGraph)
+                        targetGraph.Nodes.[toNode]
+                    | FunctionEdge e -> e(fromNode, targetGraph.Nodes.[toNode], velocity, targetGraph)
+                else
+                    fromNode
+            match a.To with
+            | StringValue s ->
+                //Cases 
+                //one step to child node child>child --pass but redundant
+                //two steps to childs child child1>child2>child2 --pass but redundant
+                //one step to parent <parent
+                //one step to parents neighbor <neighbor
+                //two steps to parents(p1) parent(p2) <<parent2
+                //two steps to parents parent's neighbor(p2n) <<p2n
+                let target = TargetParser.ExtractTarget(s)
+                let mutable targetNode = fromNode
+                for navStep in target.NavSteps do
+                    match navStep with
+                    | Up -> 
+                        match targetNode.Parent.Value with
+                        | Some a -> targetNode <- a
+                        | None -> failwith "Attempted navigation to a parent from graph root."
+                    | Down a -> 
+                        match targetNode.Value with
+                        | GraphValue g -> failwith "Attempted navigation to child node from a node with a non-graph value."
+                        | Graph g -> targetNode <- g.Nodes.[StringValue(a)]
+                    | End -> ()
+                doWalk(StringValue(target.Target), targetNode.Graph.Force())
+            | _ ->
+                doWalk(a.To, graph)
+        | ExpressionEdge a -> 
+            let action = TargetParser.ExtractTarget(x.Action)
+            if(graph.EdgeActions.ContainsKey(action.Target)) then
+                //Navigate to target node by following sequence
+                let mutable targetNode = fromNode
+                for navStep in action.NavSteps do
+                    match navStep with
+                    | Up -> 
+                        match targetNode.Parent.Value with
+                        | Some a -> targetNode <- a
+                        | None -> failwith "Attempted navigation to a parent from graph root."
+                    | Down a -> 
+                        match targetNode.Value with
+                        | GraphValue g -> failwith "Attempted navigation to child node from a node with a non-graph value."
+                        | Graph g -> targetNode <- g.Nodes.[StringValue(a)]
+                    | End -> ()
+                
+                //call registered edge
+                match graph.EdgeActions.[action.Target] with 
+                | ActionEdge e -> 
+                    e(targetNode,targetNode, velocity, graph)
+                    targetNode
+                | FunctionEdge e -> e(targetNode, targetNode, velocity, graph)
+            else
+                fromNode
+    member x.Action =
+        match x with
+        | Edge a  -> a.Action
+        | ExpressionEdge a -> a.Action
+and Node =
+    {
+        Graph: Lazy<Graph>
+        Parent: Lazy<Option<Node>>
+        Id : GraphValue
+        Edges : Map<GraphValue, EdgeLike>
         mutable Value : GraphLike
     }
+and EdgeAction =
+| ActionEdge of (Node * Node * GraphValue * Graph -> unit)
+| FunctionEdge of (Node * Node * GraphValue * Graph -> Node)
 and Graph =
     {
         Nodes : Map<GraphValue, Node>
+        EdgeActions : Map<String, EdgeAction>
     }
 
     member this.WalkEdge(fromNode : GraphValue, edge : GraphValue) =
@@ -52,10 +163,17 @@ and Graph =
 
         if this.Nodes.[fromNode].Edges.ContainsKey(edgeId) then
             let targetEdge = this.Nodes.[fromNode].Edges.[edgeId]
-            targetEdge.Action(this.Nodes.[fromNode], this.Nodes.[targetEdge.To], edge.Velocity, this)
-            this.Nodes.[targetEdge.To]
+            targetEdge.Walk(this.Nodes.[fromNode],this, edge.Velocity)
         else
             this.Nodes.[fromNode]
+
+    override this.ToString() =
+        let stringBuilder = new StringBuilder()
+        for node in this.Nodes do
+            stringBuilder.Append(node.Key.ToString()+", ")
+            |> ignore
+        stringBuilder.ToString()
+
 and GraphLike =
     | GraphValue of GraphValue
     | Graph of Graph
--- EchoGraph/EchoGraph.fs
+++ EchoGraph/EchoGraph.fs
@@ -9,43 +9,62 @@ module EchoGraph =
     let Definition =
         let edgeFunctions =
             Map.ofSeq(seq{
-                yield ("add",
-                    function(fromNode : Node, _ : Node, velocity, source) -> 
-                            let vel = GraphConversions.HandleVelocity(velocity, 1);
-                            GraphConversions.actOnGraphLikeAsInt(fromNode.Value, fun a -> fromNode.Value <- GraphConversions.assignIntAsGraphLike(a+vel))
+                yield ("add", //Only works on ints
+                    ActionEdge(
+                        function(fromNode : Node, _ , velocity, _) -> 
+                                let vel = GraphConversions.HandleVelocity(velocity, 1);
+                                GraphConversions.actOnGraphLikeAsInt(fromNode.Value, fun a -> fromNode.Value <- GraphConversions.assignIntAsGraphLike(a+vel))
+                    )
                 );
-                yield ("text",
-                    function(fromNode : Node, _ : Node, velocity, _) ->
-                            GraphConversions.actOnNodeByName(fromNode.Value, StringValue ("text"), fun a -> GraphConversions.actOnGraphLikeAsString(a.Value, fun b -> Console.WriteLine(b+velocity.ToString())))
+                yield ("text", //Only works on strings
+                    ActionEdge(
+                        function(fromNode : Node, _ , _, _) ->
+                                GraphConversions.actOnGraphLikeAsString(fromNode.Value, fun b -> Console.WriteLine(b))
+                    )
                 );
-                yield ("templateText",
-                    function(fromNode : Node, _ : Node, velocity, graph) ->
-                            let value = GraphConversions.collapseGraphLikeToString(fromNode.Value)
-                            match value with
-                            | Some v ->
-                                let mutable outputstring = v;
-                                //extract all templates from v
-                                let templates = v.Split("{", StringSplitOptions.RemoveEmptyEntries).Select(fun a ->
-                                    if(a.Contains("}")) then
-                                        a.Split("}", StringSplitOptions.RemoveEmptyEntries).First()
-                                    else
-                                        ""
-                                    )
-                                for template in templates do
-                                    if template.Contains(":") then
-                                        let splitplate = template.Split(":",StringSplitOptions.RemoveEmptyEntries)
-                                        match (splitplate.[0], splitplate.[1]) with
-                                        | ("node", _) & (var1, var2) -> 
-                                            match GraphConversions.getNodeByName(Graph(graph), StringValue(var2)) with
-                                                | Some a -> outputstring <- outputstring.Replace("{"+var1+":"+var2+"}",  GraphConversions.someOrDefault(GraphConversions.collapseGraphLikeToInt(a.Value), 0).ToString())
-                                                | None -> ()
-                                        | ("system", "velocity") & (var1, var2) -> outputstring <- outputstring.Replace("{"+var1+":"+var2+"}", GraphConversions.someOrDefault(GraphConversions.collapseGraphValueToString(velocity), "Type Cooercion Failed"))
-                                        | (_,_) -> ()
+                yield ("templateText", //Only works on strings
+                    ActionEdge(
+                        function(fromNode : Node, _, velocity, graph) ->
+                                let value = GraphConversions.collapseGraphLikeToString(fromNode.Value)
+                                match value with
+                                | Some v ->
+                                    let mutable outputstring = v;
+                                    //extract all templates from v
+                                    let templates = v.Split("{", StringSplitOptions.RemoveEmptyEntries).Select(fun a ->
+                                        if(a.Contains("}")) then
+                                            a.Split("}", StringSplitOptions.RemoveEmptyEntries).First()
+                                        else
+                                            ""
+                                        )
+                                    for template in templates do
+                                        if template.Contains(":") then
+                                            let splitplate = template.Split(":",StringSplitOptions.RemoveEmptyEntries)
+                                            match (splitplate.[0], splitplate.[1]) with
+                                            | ("node", _) & (var1, var2) -> 
+                                                match GraphConversions.getNodeByName(Graph(graph), StringValue(var2)) with
+                                                    | Some a -> outputstring <- outputstring.Replace("{"+var1+":"+var2+"}",  GraphConversions.someOrDefault(GraphConversions.collapseGraphLikeToInt(a.Value), 0).ToString())
+                                                    | None -> ()
+                                            | ("system", "velocity") & (var1, var2) -> outputstring <- outputstring.Replace("{"+var1+":"+var2+"}", GraphConversions.someOrDefault(GraphConversions.collapseGraphValueToString(velocity), "Type Cooercion Failed"))
+                                            | (_,_) -> ()
                                             
-                                Console.WriteLine(outputstring);
-                                //
-                                //loop through templates
-                            | None -> ()
+                                    Console.WriteLine(outputstring);
+                                | None -> ()
+                    )
+                );
+                yield ("assign",
+                    ActionEdge(
+                        function(fromNode : Node, _, velocity, graph) ->
+                                fromNode.Value <- GraphValue(velocity)
+                    )
+                );
+                yield ("query", //Only works on graphs of strings, uses velocity as id for nodes to walk
+                    FunctionEdge(
+                        function(fromNode : Node, _, velocity, _) ->
+                                match GraphConversions.collapseGraphLikeToGraph(fromNode.Value) with
+                                    | Some a -> 
+                                        a.WalkEdge(velocity, StringValue("queryText"))
+                                    | None -> fromNode
+                    )
                 );
             })
         let graphDefinition = ActionGraph.Load(File.ReadAllText("echoGraph.json"), edgeFunctions)
@@ -80,7 +99,7 @@ module EchoGraph =
                                         input.Walk(graph, "add=1")
                                         //teleport back to current node
                                         input.CurrentNode <- currentNode
-                                )
+                                );
                         }
                     )
             }
